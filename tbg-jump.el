d95e78e3f541af4edeffb4b2f55a922747b887eb
ACT3 <chuiC456@163.com>
2020-11-03 22:43:05 +0800

    Add tbg-jump.el & README.org files.

---
+++ tbg-jump.el
@@ -0,0 +1,21 @@
+;;; tbg-jump.el --- ▮ -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright © 2020-2027, by 3badguys
+
+;; Author: 3badguys
+;; Version: ▮
+;; Created: ▮
+;; Package-Requires: ▮
+;; Keywords: ▮
+;; License: GPL v3
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+
+;;; tbg-jump.el ends here
